a8e3a026849b802a2083d0560b49a6bf75080a22
Thefirstxerg <thefirstxerg@gmail.com>
2025-04-22 11:16:14 +0200

    Uploaded new src and updated les6

--- Code/unit1/lesson6/serve_html/main.js
+++ Code/unit1/lesson6/serve_html/main.js
@@ -1,1 +1,40 @@
-"use strict";
+const port = 3000;
+const http = require("http");
+const httpStatusCodes = require("http-status-codes");
+const router = require("./router");
+const fs = require("fs");
+
+const plainTextContentType = {
+    "Content-Type": "text/plain"
+};
+
+const htmlContentType = {
+    "Content-Type": "text/html"
+};
+
+const customReadFile = (file, res) => {
+    fs.readFile(`./${file}`, (errors, data) => {
+        if (errors) {
+            console.log("Error reading the file...");
+        }
+        res.end(data);
+    });
+};
+
+router.get("/", (req, res) => {
+    res.writeHead(httpStatusCodes.OK, plainTextContentType);
+    res.end("Still needs to be implemented");//plain text
+});
+
+router.get("/index.html", (req, res) => {
+    res.writeHead(httpStatusCodes.OK, htmlContentType);
+    customReadFile("views/index.html", res);//index.html
+});
+
+router.post("/", (req, res) => {
+    res.writeHead(httpStatusCodes.OK, plainTextContentType);
+    res.end("POSTED Succesfully");//plain text
+});
+
+http.createServer(router.handle).listen(3000);
+console.log(`The server is listening on port number: ${port}`);
--- Code/unit1/lesson6/serve_html/router.js
+++ Code/unit1/lesson6/serve_html/router.js
@@ -1,1 +1,42 @@
-"use strict";
+const httpStatus = require("http-status-codes");
+
+const htmlContentType = {
+    "Content-Type": "text/html"
+};
+
+const routes = {
+    "GET": {
+        "/info": (req, res) => {
+            res.writeHead(httpStatus.OK, htmlContentType, {
+                "Content-Type": "text/plain"
+            })
+            res.end("<h1>Welcome to the Info Page!<h1><p>This is the info page.</p>");
+        },
+        "/about": (req, res) => {
+            res.writeHead(httpStatus.OK, htmlContentType, {
+                "Content-Type": "text/plain"
+            });
+            res.end("<h1>About</h1><p>This is the about page.</p>");
+        },
+
+    },
+    'POST': {}
+};
+exports.handle = (req, res) => {
+    try {
+        if (routes[req.method][req.url]) {
+            routes[req.method][req.url](req, res);
+        } else {
+            res.writeHead(httpStatus.NOT_FOUND, htmlContentType);
+            res.end("<h1>No such file exists</h1>");
+        }
+    } catch (ex) {
+        console.log("error: " + ex);
+    }
+};
+exports.get = (url, action) => {
+    routes["GET"][url] = action;
+};
+exports.post = (url, action) => {
+    routes["POST"][url] = action;
+};
